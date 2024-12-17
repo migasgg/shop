bbe64c3767a8dd6da65c5afc6f15e98c27b13cbd
migas <miguelalbert38@gmail.com>
2024-12-17 19:52:15 -0300

    styled header and home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { globalStyled } from "@/styles/global"
 import type { AppProps } from "next/app"
+import imgLogo from "../assets/logo.svg"
+import Image from "next/image"
+import { Container, Header } from "@/styles/pages/app"
 
 globalStyled()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={imgLogo} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
+
+import camiseta1 from "../assets/shirts/1.png"
+import camiseta2 from "../assets/shirts/2.png"
+//import camiseta3 from "../assets/shirts/3.png"
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
