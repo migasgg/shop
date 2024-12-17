d369f2ec08138b5611e0889e962c71263a92d1f4
migas <miguelalbert38@gmail.com>
2024-12-17 15:16:50 -0300

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { globalStyled } from "@/styles/global"
+import type { AppProps } from "next/app"
+
+globalStyled()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
